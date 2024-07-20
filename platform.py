0b170b8b29f2ea17329f4c2b00783b86feee21cd
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-07-20 16:00:08 +0200

    disable install of tool-mconf / tool-idf

--- platform.py
+++ platform.py
@@ -124,8 +124,8 @@ class Espressif32Platform(PlatformBase):
             for p in self.packages:
                 if p in ("tool-cmake", "tool-ninja"):
                     self.packages[p]["optional"] = False
-                elif p in ("tool-mconf", "tool-idf") and IS_WINDOWS:
-                    self.packages[p]["optional"] = False
+#                elif p in ("tool-mconf", "tool-idf") and IS_WINDOWS:
+#                    self.packages[p]["optional"] = False
 
         for available_mcu in ("esp32", "esp32s2", "esp32s3"):
             if available_mcu == mcu:
