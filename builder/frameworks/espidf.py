3bc3ac7cb023138217d8624d063f4690778dc77d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-07-30 14:02:09 +0200

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -644,30 +644,16 @@ def generate_project_ld_script(sdk_config, ignore_targets=None):
         '--objdump "{objdump}"'
     ).format(**args)
 
-    initial_ld_script = os.path.join(
-        FRAMEWORK_DIR,
-        "components",
-        "esp_system",
-        "ld",
-        idf_variant,
-        "sections.ld.in",
-    )
-
-    if IDF5:
-        initial_ld_script = preprocess_linker_file(
-            initial_ld_script,
-            os.path.join(
-                BUILD_DIR,
-                "esp-idf",
-                "esp_system",
-                "ld",
-                "sections.ld.in",
-            )
-        )
-
     return env.Command(
         os.path.join("$BUILD_DIR", "sections.ld"),
-        initial_ld_script,
+        os.path.join(
+            FRAMEWORK_DIR,
+            "components",
+            "esp_system",
+            "ld",
+            idf_variant,
+            "sections.ld.in",
+        ),
         env.VerboseAction(cmd, "Generating project linker script $TARGET"),
     )
 
@@ -1117,46 +1103,6 @@ def get_app_partition_offset(pt_table, pt_offset):
     return app_params.get("offset", "0x10000")
 
 
-def preprocess_linker_file(src_ld_script, target_ld_script):
-    return env.Command(
-        target_ld_script,
-        src_ld_script,
-        env.VerboseAction(
-            " ".join(
-                [
-                    os.path.join(
-                        platform.get_package_dir("tool-cmake"),
-                        "bin",
-                        "cmake",
-                    ),
-                    "-DCC=%s"
-                    % os.path.join(
-                        TOOLCHAIN_DIR,
-                        "bin",
-                        "$CC",
-                    ),
-                    "-DSOURCE=$SOURCE",
-                    "-DTARGET=$TARGET",
-                    "-DCONFIG_DIR=%s" % os.path.join(BUILD_DIR, "config"),
-                    "-DLD_DIR=%s"
-                    % os.path.join(
-                        FRAMEWORK_DIR, "components", "esp_system", "ld"
-                    ),
-                    "-P",
-                    os.path.join(
-                        "$BUILD_DIR",
-                        "esp-idf",
-                        "esp_system",
-                        "ld",
-                        "linker_script_generator.cmake",
-                    ),
-                ]
-            ),
-            "Generating LD script $TARGET",
-        ),
-    )
-
-
 def generate_mbedtls_bundle(sdk_config):
     bundle_path = os.path.join("$BUILD_DIR", "x509_crt_bundle")
     if os.path.isfile(env.subst(bundle_path)):
@@ -1403,30 +1349,19 @@ generate_default_component()
 #
 
 if not board.get("build.ldscript", ""):
-    initial_ld_script = board.get("build.esp-idf.ldscript", os.path.join(
-        FRAMEWORK_DIR,
-        "components",
-        "esp_system",
-        "ld",
-        idf_variant,
-        "memory.ld.in",
-    ))
-
-    if IDF5:
-        initial_ld_script = preprocess_linker_file(
-            initial_ld_script,
-            os.path.join(
-                BUILD_DIR,
-                "esp-idf",
-                "esp_system",
-                "ld",
-                "memory.ld.in",
-            )
-        )
-
     linker_script = env.Command(
         os.path.join("$BUILD_DIR", "memory.ld"),
-        initial_ld_script,
+        board.get(
+            "build.esp-idf.ldscript",
+            os.path.join(
+                FRAMEWORK_DIR,
+                "components",
+                "esp_system",
+                "ld",
+                idf_variant,
+                "memory.ld.in",
+            ),
+        ),
         env.VerboseAction(
             '$CC -I"$BUILD_DIR/config" -I"%s" -C -P -x c -E $SOURCE -o $TARGET'
             % os.path.join(FRAMEWORK_DIR, "components", "esp_system", "ld"),
@@ -1777,7 +1712,7 @@ env["BUILDERS"]["ElfToBin"].action = action
 #
 
 ulp_dir = os.path.join(PROJECT_DIR, "ulp")
-if os.path.isdir(ulp_dir) and os.listdir(ulp_dir) and mcu not in ("esp32c2", "esp32c3", "esp32c6", "esp32h2"):
+if os.path.isdir(ulp_dir) and os.listdir(ulp_dir) and mcu not in ("esp32c2", "esp32c3", "esp32h2"):
     env.SConscript("ulp.py", exports="env sdk_config project_config idf_variant")
 
 #
